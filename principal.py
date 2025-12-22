258468b997d4a94ab0e6ccd866553aa49bc59e33
Javier <javier@solvam.es>
2025-12-22 10:26:22 +0100

    Corrección pociones/pruebas

--- principal.py
+++ principal.py
@@ -14,10 +14,12 @@ vida = pocion.Pocion("Recuperación de vida", 10)
 daño = pocion.Pocion("Daño", 10)
 escudo = pocion.Pocion("Escudo", 10)
 
-# Pruebas para ver si las pociones funcionan
-guerrero = Guerrero('Cadalas')
+# Pruebas para ver si las pociones funcionan. La poción tiene el método tomar_pocion, recibe un personaje y modifica directamente sus atributos según el tipo de poción.
+guerrero = Guerrero("Cadalas")
 
-print(guerrero.tomar_pocion())
+print("Vida antes:", guerrero.vida)
+vida.tomar_pocion(guerrero)
+print("Vida después:", guerrero.vida)
 
 """
 def menu():
